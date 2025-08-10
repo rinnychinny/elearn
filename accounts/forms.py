d1dc43088cc805adcc0312035657c2a435234c4d
arpy <richpemuk@gmail.com>
2025-08-10 10:28:54 +0200

    added bootstrap and crispy forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from .models import UserProfile
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 # form for viewing/updating user profile information
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['public_name', 'public_status', 'public_bio']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'  # Use POST method for form
+        self.helper.add_input(Submit('submit', 'Save Profile'))  # Add submit button
--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    public_name = models.TextField(unique=True, null=False, blank=False)
-    public_status = models.TextField(unique=False, null=False, blank=True,  default = '')
+    public_name = models.CharField(unique=True, null=False, blank=False, max_length=128)
+    public_status = models.CharField(unique=False, null=False, blank=True,  default = '', max_length=256)
     public_bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
