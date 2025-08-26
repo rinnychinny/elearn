f0ce8791c50b8704573be98717bfbd39bda1046c
arpy <richpemuk@gmail.com>
2025-08-26 13:02:30 +0200

    added user search functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,17 @@ from . import views
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('register/', views.student_register,
          name='student_register'),  # Registration
     # Post-login landing page
     path('dashboard/', views.role_landing, name='role_landing'),
     path('profile/', views.profile_view, name='profile'),
+    path('user_search/', views.user_search, name='user_search'),
+    # public profile page by user ID
+    path('users/<int:user_id>/', views.public_profile, name='public_profile'),
 
     path(
         "password_change/",
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
@@ -24,6 +25,28 @@ def student_register(request):
 
 
 @login_required
+def user_search(request):
+    query = request.GET.get('q', '').strip()
+    results = []
+
+    if query:
+        # Search public_name
+        results = UserProfile.objects.filter(
+            public_name__icontains=query).select_related('user')
+        for profile in results:
+            user_groups = profile.user.groups.values_list('name', flat=True)
+            profile.user_groups_csv = ", ".join(user_groups)
+            profile.is_teacher = 'Teacher' in user_groups
+            profile.is_student = 'Student' in user_groups
+
+    context = {
+        "query": query,
+        "results": results,
+    }
+    return render(request, "accounts/user_search.html", context)
+
+
+@login_required
 def profile_view(request):
     user = request.user
     profile, created = UserProfile.objects.get_or_create(
@@ -36,6 +59,16 @@ def profile_view(request):
     else:
         form = UserProfileForm(instance=profile)
     return render(request, 'accounts/profile_page.html', {'profile_form': form})
+
+
+@login_required
+def public_profile(request, user_id):
+    profile = get_object_or_404(UserProfile, user__id=user_id)
+    user_groups = profile.user.groups.values_list('name', flat=True)
+    profile.user_groups_csv = ", ".join(user_groups)
+    profile.is_teacher = 'Teacher' in user_groups
+    profile.is_student = 'Student' in user_groups
+    return render(request, 'accounts/public_profile.html', {'profile': profile})
 
 
 @login_required
