8732c8492593549da9eabf2b8181f32b4ee6d5f9
arpy <richpemuk@gmail.com>
2025-08-26 11:11:59 +0200

    updated dumpdata

--- dumpdata.sh
+++ dumpdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python manage.py dumpdata --exclude auth.permission --exclude contenttypes --indent 2 > db.json
+python manage.py dumpdata --exclude auth.permission --exclude contenttypes --exclude admin.logentry --exclude sessions.session --indent 2 > db.json
 echo "Data dumped to db.json"
