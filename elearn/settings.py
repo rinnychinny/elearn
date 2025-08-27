1dd6cc91232dca47eb2eec761592a79cc8b0907b
arpy <richpemuk@gmail.com>
2025-08-27 14:00:17 +0200

    updating requirements

--- elearn/settings.py
+++ elearn/settings.py
@@ -223,3 +223,11 @@ LOGGING = {
         },
     },
 }
+
+# Celery (with namespace 'CELERY' via celery.py)
+CELERY_BROKER_URL = os.getenv("REDIS_URL")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = TIME_ZONE
