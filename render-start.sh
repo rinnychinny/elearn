66d1f7247d7a333d3e77e4c9c96fb7fdddfe075d
arpy <richpemuk@gmail.com>
2025-08-27 10:47:34 +0200

    edited render start script for user_init

--- render-start.sh
+++ render-start.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 echo "[start] applying migrations…"
 python manage.py migrate --noinput
+python manage.py user_init 
 
 echo "[start] launching daphne…"
 exec daphne -b 0.0.0.0 -p "$PORT" elearn.asgi:application
