129eafedbcec413462c60d49fee101046a94fd50
arpy <richpemuk@gmail.com>
2025-08-29 21:22:16 +0200

    added ERD

--- elearn/settings.py
+++ elearn/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'crispy_forms',
     'crispy_bootstrap5',
     'accounts',
